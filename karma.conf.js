2edc6a1158042076f1e97e9840c74b43d8405a52
mari-borges <marianabrgs77@gmail.com>
2021-07-28 17:06:56 -0300

    Inserindo o coverage, para ter cubertura dos testes

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
